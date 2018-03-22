b19b752a66821be2a56e240bdf20286e81d0c2d9
Aaron <aaronngcx@gmail.com>
2018-03-22 22:27:11 +0800

    newjob fix
    newjob fix

--- newjob.php
+++ newjob.php
@@ -52,171 +52,212 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 ?>
 
-<!DOCTYPE HTML>
-<html lang="en">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>New Job</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Jinjang Utara Community Website</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link rel="stylesheet" href="assets/style.css" />
-<script src="assets/jquery3.js"></script>
-<script src="assets/scripts.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" href="css/prettyPhoto.css" />
+<link rel="stylesheet" href="css/flexslider.css" />
+<link rel="stylesheet" href="css/custom-styles.css">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" href="css/style-ie.css"/>
+<![endif]-->
+
+<!-- Favicons
+================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- JS
+================================================== -->
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/jquery.custom.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+
+    $("#btn-blog-next").click(function () {
+      $('#blogCarousel').carousel('next')
+    });
+     $("#btn-blog-prev").click(function () {
+      $('#blogCarousel').carousel('prev')
+    });
+
+     $("#btn-client-next").click(function () {
+      $('#clientCarousel').carousel('next')
+    });
+     $("#btn-client-prev").click(function () {
+      $('#clientCarousel').carousel('prev')
+    });
+
+});
+
+ $(window).load(function(){
+
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+    });
+});
+
+</script>
+
 </head>
-<body>
+
+<body class="home">
+  <div class="container-fluid">
+    <div class="row w">
+      <div class="offset1 span5">
+        <a href="index.php"><div class="logo"></div></a>
+      </div>
+      <div class="span8 navigation">
+          <div class="navbar hidden-phone">
 
 
+          <ul class="nav">
+           <li class="dropdown active"><a href="index.php">Home</a></li>
+           <li><a href="signup.php">Sign Up</a></li>
+           <li><a href="login.php">Log In</a></li>
+           <li><a href="newjob.php">Jobs</a></li>
+          </ul>
+          </div>
 
-<header>
-<div class="page-width">
-
-<a class="logo" href="index.php">&nbsp;</a>
-
-<nav>
-
-<div class="menubutton"></div>
-
-
-
-
-<ul class="menubtns">
-<li ><a href="index.php">Home</a></li>
-<li class="selected"><a href="newjob.php">List a Job</a></li>
-<li><a href="">Jobs Listed</a></li>
-<li><a href="donate.php">Donate</a></li>
-<li><a href="contact.php">Contact us</a></li>
-<li><a href="">Profile</a></li>
-<li><a href="index.php">Logout</a></li>
-</ul>
-</header>
-
-<div class="container">
-
-<br><br><br><br>
-
-<div class="page-width">
-
-<div class="memberform">
-
-	<form action="newjob.php" method="POST" class="registerform">
-		<h2>New Job Form</h2>
-		<div class="formelement">
-			<label for="jobtitle">Job Title:</label>
-			<input type="text" id="jobtitle" name="jobtitle" value="" placeholder="" required />
-		</div>
-		<div class="formelement">
-			<label for="jobduration">Job Duration:</label>
-			<input type="text" id="jobduration" name="jobduration" value="" placeholder="" required />
-		</div>
-		<div class="formelement1">
-			<label for="jobtime">Job Time:</label>
-			<input type="time" id="startime" name="startime" value="" required/>
-			to
-			<input type="time" id="endtime" name="endtime" value="" required/>
-		</div>
-		<div class="formelement">
-			<label for="jobdescription">Job Description</label>
-			<textarea rows="5" cols="50" id="jobdescription" name="jobdescription" value="" placeholder="" required> </textarea>
-		</div>
-		<div class="formelement">
-			<label for="Number of Workers">Number of Workers:</label>
-			<input type="number" min="1" id="workernumber" name="workernumber" value="" placeholder="" required />
-		</div>
-		<div class="formelement">
-			<label for="Salary">Salary (RM per hour):</label>
-			<input type="number" min="1" id="salary" name="salary" value="" placeholder="<?php if(isset($salaryError)){echo $salaryError;} ?>" required />
-		</div>
-		<div class="formelement">
-			<label for="contactinfo">Contact Information:</label>
-			<input type="text" id="contactinfo" name="contactinfo" value="" placeholder="<?php if(isset($workernumberError)){echo $workernumberError;} ?>" required />
-		</div>
-		<div class="formelement">
-		<input type="submit" class="button" value="Create New Job" />
-		</div>
-		<div class="clear"></div>
-	</form>
-
-
-
-</div>
-
-</div>
-
-
-
-</div>
-
-
-
-<div class="footer-placeholder">&nbsp;</div>
-<footer class="footer-distributed">
-
-  <div class="footer-left">
-
-    <h3>Jinjang Utara Community Website</span></h3>
-
-    <p class="footer-links">
-      <a href="#">Blog</a>
-      ·
-      <a href="#">About</a>
-      ·
-      <a href="#">FAQ</a>
-      ·
-      <a href="#">Contact</a>
-    </p>
-
-    <p class="footer-company-name">AGN &copy; 2015</p>
+      </div>
+    </div>
   </div>
 
-  <div class="footer-center">
 
-    <div>
-      <i class="fa fa-map-marker"></i>
-      <p><span>Jinjang Utara</span> 52000, Kuala Lumpur</p>
-    </div>
+	
 
-    <div>
-      <i class="fa fa-phone"></i>
-      <p>03-2716 2000</p>
-    </div>
+	<div class="page-width">
 
-    <div>
-      <i class="fa fa-envelope"></i>
-      <p><a href="mailto:support@jucw.com">support@jucw.com</a></p>
-    </div>
+	<div class="memberform">
 
-  </div>
+		<form action="newjob.php" method="POST" class="registerform">
+			<h2>New Job Form</h2>
+			<div class="formelement">
+				<label for="jobtitle">Job Title:</label>
+				<input type="text" id="jobtitle" name="jobtitle" value="" placeholder="" required />
+			</div>
+			<div class="formelement">
+				<label for="jobduration">Job Duration:</label>
+				<input type="text" id="jobduration" name="jobduration" value="" placeholder="" required />
+			</div>
+			<div class="formelement1">
+				<center><label for="jobtime">Job Time:</label>
+				<input type="time" id="startime" name="startime" value="" required/>
+				<br>
+				<center>to<center>
+				<br>
+				<input type="time" id="endtime" name="endtime" value="" required/>
+			</div>
+			<div class="formelement">
+				<label for="jobdescription">Job Description</label>
+				<textarea rows="5" cols="50" id="jobdescription" name="jobdescription" value="" placeholder="" required> </textarea>
+			</div>
+			<div class="formelement">
+				<label for="Number of Workers">Number of Workers:</label>
+				<input type="number" min="1" id="workernumber" name="workernumber" value="" placeholder="" required />
+			</div>
+			<div class="formelement">
+				<label for="Salary">Salary (RM per hour):</label>
+				<input type="number" min="1" id="salary" name="salary" value="" placeholder="<?php if(isset($salaryError)){echo $salaryError;} ?>" required />
+			</div>
+			<div class="formelement">
+				<label for="contactinfo">Contact Information:</label>
+				<input type="text" id="contactinfo" name="contactinfo" value="" placeholder="<?php if(isset($workernumberError)){echo $workernumberError;} ?>" required />
+			</div>
+			<div class="formelement">
+			<input type="submit" class="button" value="Create New Job" />
+			</div>
+			<div class="clear"></div>
+		</form>
 
-  <div class="footer-right">
-
-    <p class="footer-company-about">
-      <span>About the company</span>
-       AGN strives to better the lives of the people within Jinjang Utara Community
-    </p>
-
-    <div class="footer-icons">
-
-      <a href="#"><i class="fa fa-facebook"></i></a>
-      <a href="#"><i class="fa fa-twitter"></i></a>
-      <a href="#"><i class="fa fa-linkedin"></i></a>
-      <a href="#"><i class="fa fa-github"></i></a>
-
-    </div>
-
-  </div>
-
-</footer>
 
 
-    <div class="footer-icons">
+	</div>
 
-      <a href="#"><i class="fa fa-facebook"></i></a>
-      <a href="#"><i class="fa fa-twitter"></i></a>
-      <a href="#"><i class="fa fa-linkedin"></i></a>
-      <a href="#"><i class="fa fa-github"></i></a>
+	</div>
 
-    </div>
+    <!-- Footer Area
+        ================================================== -->
+        <footer class="footer-distributed">
 
-  </div>
+          <div class="footer-left">
 
-</footer>
+            <h3>Jinjang Utara Community Website</span></h3>
+
+            <p class="footer-links">
+              <a href="#">Blog</a>
+              ·
+              <a href="#">About</a>
+              ·
+              <a href="#">FAQ</a>
+              ·
+              <a href="#">Contact</a>
+            </p>
+
+            <p class="footer-company-name">AGN &copy; 2015</p>
+          </div>
+
+          <div class="footer-center">
+
+            <div>
+              <i class="fa fa-map-marker"></i>
+              <p><span>Jinjang Utara</span> 52000, Kuala Lumpur</p>
+            </div>
+
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>03-2716 2000</p>
+            </div>
+
+            <div>
+              <i class="fa fa-envelope"></i>
+              <p><a href="mailto:support@jucw.com">support@jucw.com</a></p>
+            </div>
+
+          </div>
+
+          <div class="footer-right">
+
+            <p class="footer-company-about">
+              <span>About the company</span>
+               AGN strives to better the lives of the people within Jinjang Utara Community
+            </p>
+
+            <div class="footer-icons">
+
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-linkedin"></i></a>
+              <a href="#"><i class="fa fa-github"></i></a>
+
+            </div>
+
+          </div>
+
+        </footer>
+
+
+    <!-- Scroll to Top -->
+    <div id="toTop" class="hidden-phone hidden-tablet">Back to Top</div>
 
 </body>
 </html>
